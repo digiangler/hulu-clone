c8351590090862ea6d9b256da71e9132468ab10b
Digi-Angler <digiangler777@gmail.com>
2020-09-27 17:10:08 +0900

    First commit 🚀

---
+++ .history/src/requests_20200927163642.js
@@ -0,0 +1,15 @@
+const API_KEY = "8712b0610496fec8b39847307d977e5f";
+
+export default {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=ja-JP`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=ja-JP`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHororMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchSciFiMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchWesternMovies: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchTv: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+};
---
+++ src/components/VideoCard.js
@@ -0,0 +1,38 @@
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+
+import React, { forwardRef } from "react";
+
+import TextTruncate from "react-text-truncate";
+
+import "./VideoCard.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const VideoCard = forwardRef(({ movie }, ref) => {
+  return (
+    <div ref={ref} className="videoCard">
+      <img
+        src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+        alt="movie poster"
+      />
+      <TextTruncate
+        line={1}
+        element="p"
+        truncateText="..."
+        text={movie.overview}
+      />
+      <TextTruncate
+        line={1}
+        element="h2"
+        truncateText="..."
+        text={movie.title || movie.original_name}
+      />
+      <p className="videoCard__stats">
+        {movie.release_date || movie.first_air_date} ·{" "}
+        <ThumbUpAltIcon className="thumb" /> {movie.vote_count}
+      </p>
+    </div>
+  );
+});
+
+export default VideoCard;
